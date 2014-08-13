86259befe289abc73fc6df1aee5f2890430d4423
Joe Ferner <joe@fernsroth.com>
2014-08-13 19:49:20 -0400

    remove kicad-library as subtree

--- pull-kicad-library.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-git subtree pull --prefix=kicad-library git@github.com:joeferner/kicad-library.git master
-
--- update-from-kicad-library.sh
+++ update-from-kicad-library.sh
@@ -4,7 +4,7 @@ echo "Sync'ing mods"
 for f in mods/*
 do
   FILENAME=$(basename $f)
-  KICADLIB_FILENAME=./kicad-library/mods/${FILENAME}
+  KICADLIB_FILENAME=../kicad-library/mods/${FILENAME}
   LOCAL_FILENAME=./mods/${FILENAME}
   if [ -f ${KICADLIB_FILENAME} ]; then
     cp ${KICADLIB_FILENAME} ${LOCAL_FILENAME}
@@ -13,4 +13,4 @@ do
   fi
 done
 
-kicad-update -i stm32-48pin-devBoard.lib.list -o stm32-48pin-devBoard.lib --basedir ./kicad-library
+kicad-update -i stm32-48pin-devBoard.lib.list -o stm32-48pin-devBoard.lib --basedir ../kicad-library
