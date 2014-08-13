8602d71b636d18737b353c02c945a23569c7da2f
Joe Ferner <joe@fernsroth.com>
2014-08-13 19:33:55 -0400

    upgrade to new kicad mod layout

--- update-from-kicad-library.sh
+++ update-from-kicad-library.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 
-kicad-update -i stm32-48pin-devBoard.mod.list -o stm32-48pin-devBoard.mod --basedir ./kicad-library
+echo "Sync'ing mods"
+for f in mods/*
+do
+  FILENAME=$(basename $f)
+  KICADLIB_FILENAME=./kicad-library/mods/${FILENAME}
+  LOCAL_FILENAME=./mods/${FILENAME}
+  if [ -f ${KICADLIB_FILENAME} ]; then
+    cp ${KICADLIB_FILENAME} ${LOCAL_FILENAME}
+  else
+    echo "Could not find ${KICADLIB_FILENAME}"
+  fi
+done
+
 kicad-update -i stm32-48pin-devBoard.lib.list -o stm32-48pin-devBoard.lib --basedir ./kicad-library
